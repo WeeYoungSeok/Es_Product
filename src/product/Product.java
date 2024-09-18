940e131ccdd59c92166c42df3be33c10852cd078
WeeYoungSeok <dndrldzk@gmail.com>
2024-09-19 02:03:18 +0900

    feat: Product 클래스 추가

--- src/product/Product.java
+++ src/product/Product.java
@@ -3,6 +3,19 @@ package product;
 import event.Promotion;
 
 public class Product implements Promotion {
+    String name;
+    int price;
+    int weight;
+
+    public Product() {
+    }
+
+    public Product(String name, int price, int weight) {
+        this.name = name;
+        this.price = price;
+        this.weight = weight;
+    }
+
     @Override
     public int getDiscountAmount() {
         return 0;
