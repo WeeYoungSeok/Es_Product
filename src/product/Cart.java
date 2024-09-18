8554acce81e954c654aaca65a1bde9060cfd8ee4
WeeYoungSeok <dndrldzk@gmail.com>
2024-09-19 02:42:55 +0900

    feat: 배송비 관련 로직 추가

--- src/product/Cart.java
+++ src/product/Cart.java
@@ -1,15 +1,48 @@
 package product;
 
-import java.util.List;
+import delivery.DeliveryChargeCalculator;
 
-public class Cart {
+import java.math.BigDecimal;
+
+public class Cart implements DeliveryChargeCalculator {
     private Product[] products;
 
     public Cart(Product[] products) {
         this.products = products;
     }
+    @Override
+    public BigDecimal getDeliveryCharge(Double weight, BigDecimal price) {
+        BigDecimal deliveryAmount;
+        if (weight < 3d) {
+            deliveryAmount = new BigDecimal("1000");
+        } else if (weight >= 3d && weight < 10d) {
+            deliveryAmount = new BigDecimal("5000");
+        } else {
+            deliveryAmount = new BigDecimal("10000");
+        }
+
+        BigDecimal deliveryCharge;
+        if (price.compareTo(new BigDecimal("30000")) < 0) {
+            deliveryCharge = new BigDecimal("0");
+        } else if (price.compareTo(new BigDecimal("30000")) >= 0 &&
+        price.compareTo(new BigDecimal("100000")) < 0) {
+            deliveryCharge = new BigDecimal("1000");
+        } else {
+            deliveryCharge = deliveryAmount;
+        }
+
+        return deliveryAmount.subtract(deliveryCharge);
+    }
 
     public int calculateDeliveryCharge() {
-        return 0;
+        BigDecimal totalCharge = BigDecimal.ZERO;
+
+        // 모든 제품의 배송 요금을 합산
+        for (Product product : products) {
+            totalCharge = totalCharge.add(getDeliveryCharge(product.getWeight(), product.getPrice()));
+        }
+
+        return totalCharge.intValue();
     }
+
 }
