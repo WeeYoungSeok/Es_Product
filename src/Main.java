d270c33ca27fc3a2bdba31f88d9c745fe47a42cf
WeeYoungSeok <dndrldzk@gmail.com>
2024-09-19 02:49:04 +0900

    feat: Main 클래스 구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import product.*;
+
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product beauty = new Beauty("beauty", new BigDecimal("30000"), 2d);
+        Product grocery = new Grocery("grocery", new BigDecimal("20000"), 3d);
+        Product largeAppliance = new LargeAppliance("largeAppliance", new BigDecimal("50000"), 5d);
+
+        Cart cart = new Cart(new Product[] {beauty, grocery, largeAppliance});
+        int totalDeliveryCharge = cart.calculateDeliveryCharge();
+        System.out.println(totalDeliveryCharge);    // 결과: 9000
     }
 }
