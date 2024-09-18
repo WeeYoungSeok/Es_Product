f68c5a3b0def5a60556d9f9bfd343cacbec36d5d
WeeYoungSeok <dndrldzk@gmail.com>
2024-09-19 02:19:40 +0900

    fix: Cart 생성자 추가

--- src/product/Cart.java
+++ src/product/Cart.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class Cart {
     private Product[] products;
 
+    public Cart(Product[] products) {
+        this.products = products;
+    }
+
     public int calculateDeliveryCharge() {
         return 0;
     }
