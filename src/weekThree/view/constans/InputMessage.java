b15bf8627a48ebcda1d251d5d1b3ec0336b3211b
WeeYoungSeok <dndrldzk@gmail.com>
2024-09-20 18:28:05 +0900

    fix: 인풋 메세지 종료 문자열 수정

--- src/weekThree/view/constans/InputMessage.java
+++ src/weekThree/view/constans/InputMessage.java
@@ -5,7 +5,7 @@ public enum InputMessage {
     PRIVATE_CONTACT_ADD("2. 개인 연락처 추가"),
     CONTACT_PRINT("3. 연락처 출력"),
     CONTACT_SEARCH("4. 연락처 검색"),
-    EXIT("종료"),
+    EXIT("5. 종료"),
     MENU_CHOICE("메뉴를 선택해주세요: ↵"),
 
     // 비지니스 연락처 추가
