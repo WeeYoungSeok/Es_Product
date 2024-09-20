5c13e64ebbf6dc84ba2c1a0608e4aa516e4f9093
WeeYoungSeok <dndrldzk@gmail.com>
2024-09-20 17:37:57 +0900

    feat: 정규식 enum 추가

---
+++ src/weekThree/contants/Regx.java
@@ -0,0 +1,15 @@
+package weekThree.contants;
+
+public enum Regx {
+    PHONE_NUMBER_REGX("^01[1-9]-\\d{3,4}-\\d{4}$");
+
+    private String pattern;
+
+    Regx(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+}
