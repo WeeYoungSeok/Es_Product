2f2066d1e4a7c931e3fa7ec970bf71cc30c341a7
WeeYoungSeok <dndrldzk@gmail.com>
2024-09-20 17:38:13 +0900

    feat: 생성자에서 validation 진행 코드 추가

--- src/weekThree/domain/Contact.java
+++ src/weekThree/domain/Contact.java
@@ -1,12 +1,32 @@
 package weekThree.domain;
 
+import weekThree.contants.Regx;
+import weekThree.validation.StringValidation;
+import weekThree.view.constans.ErrorMessage;
+
 public class Contact {
     private String name;
     private String phoneNumber;
 
     public Contact(String name, String phoneNumber) {
+        validation(name, phoneNumber);
         this.name = name;
         this.phoneNumber = phoneNumber;
+    }
+
+    private void validation(String name, String phoneNumber) {
+        // 빈 값 검사
+        StringValidation.isEmpty(name);
+        StringValidation.isEmpty(phoneNumber);
+        // 전화 번호 형식 검사
+        phoneNumberValidation(phoneNumber);
+
+    }
+
+    private void phoneNumberValidation(String phoneNumber) {
+        if (!Regx.PHONE_NUMBER_REGX.getPattern().matches(phoneNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_PHONE_NUMBER_FORMAT.getMessage());
+        }
     }
 
     public String getName() {
