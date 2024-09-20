71d2a20aa76d1214b9df486007b580f65474f243
WeeYoungSeok <dndrldzk@gmail.com>
2024-09-20 20:18:54 +0900

    fix: 메세지 중복 삭제

--- src/weekThree/view/constans/InputMessage.java
+++ src/weekThree/view/constans/InputMessage.java
@@ -3,14 +3,14 @@ package weekThree.view.constans;
 public enum InputMessage {
     MENU_CHOICE("메뉴를 선택해주세요: ↵"),
 
-    // 비지니스 연락처 추가
-    BUSINESS_NAME("이름을 입력하세요: ↵"),
-    BUSINESS_CONTACT("전화번호를 입력하세요: ↵"),
+    // 이름 및 연락처
+    NAME("이름을 입력하세요: ↵"),
+    CONTACT("전화번호를 입력하세요: ↵"),
+
+    // 비지니스
     BUSINESS_COMPANY_NAME("회사명을 입력하세요: ↵"),
 
-    // 개인 연락처 추가
-    PRIVATE_NAME("이름을 입력하세요: ↵"),
-    PRIVATE_CONTACT("전화번호를 입력하세요: ↵"),
+    // 개인
     PRIVATE_RELATION("이름을 입력하세요: ↵"),
 
     // 연락처 검색
